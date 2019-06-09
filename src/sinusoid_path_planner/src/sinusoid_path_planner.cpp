923adc9e9df4b13ca64c69764ff09e0d8da9a24b
M@t <msy22@uclive.ac.nz>
2019-06-10 10:14:10 +1200

    Removed the intermediate goals which were bugged
    Boggles the mind that this code was put in a tutorial because it simply doesn't work.

--- src/sinusoid_path_planner/src/sinusoid_path_planner.cpp
+++ src/sinusoid_path_planner/src/sinusoid_path_planner.cpp
@@ -41,20 +41,9 @@ namespace SinusoidPathPlanner
                                std::vector<geometry_msgs::PoseStamped>& plan )
   {
     plan.push_back(start);
-    for (int i=0; i<20; i++)
     {
-      geometry_msgs::PoseStamped new_goal = goal;
-      tf::Quaternion goal_quat = tf::createQuaternionFromYaw(1.54);
 
-      new_goal.pose.position.x = -2.5+(0.05*i);
-      new_goal.pose.position.y = -3.5+(0.05*i);
 
-      new_goal.pose.orientation.x = goal_quat.x();
-      new_goal.pose.orientation.y = goal_quat.y();
-      new_goal.pose.orientation.z = goal_quat.z();
-      new_goal.pose.orientation.w = goal_quat.w();
-
-      plan.push_back(new_goal);
     }
     plan.push_back(goal);
     return true;
